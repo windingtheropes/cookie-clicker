cc4373b2efc2f40b8911b2d4f9d5ef0593c95ed5
Jack Anderson <Jack Anderson>
2024-01-23 12:32:11 -0500

    more

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
 const debug = true
 
+// number formatter
+class Num {
+  constructor(val) {
+    this.value = val
+    this.number_names = {
+      1_000_000_000_000_000: "quadrillion",
+      1_000_000_000_000: "trillion",
+      1_000_000_000: "billion",
+      1_000_000: "million",
+    }
+  }
+
+  // test for the biggest number which this.value is greater than to most accurately name it
+  findBiggest() {
+    let n = undefined;
+    for(const num in this.number_names) {
+      if(this.value >= parseFloat(num)) {
+        n = num
+      }
+    }
+    return n
+  }
+  named(accuracy = 1) {
+    const biggest = this.findBiggest()
+    if(!biggest) return this.value
+    return `${(this.value / this.findBiggest()).toFixed(accuracy)} ${this.number_names[this.findBiggest()]}` || this.value
+  }
+}
+
 class Upgrade {
-  constructor(name, description, price, run) {
+  constructor(name, description, price, run, spriteCoords=[0,0]) {
     this.name = name;
+    this.refname = name.toLowerCase().split(" ").join("")
     this.description = description;
     this.price = price;
     this.run = run;
-
     this.owned = false;
 
-    this.id = `buyupgrade-${this.name.toLowerCase()}`
+    this.id = `buyupgrade-${this.refname}`
     this.store_html = 
     `
-    <a id="buyupgrade-${this.name.toLowerCase()}" class="upgrade ${this.name.toLowerCase()}" title="${this.description}">
-        <div class="upgrade-img"></div>
+    <a id="${this.id}" class="upgrade ${this.refname}" title="${this.description} | ${this.price} cookies">
+        <div class="upgrade-img" style="background: url('https://cdn.dashnet.org/cookieclicker/img/icons.png?v=2.052') -${spriteCoords[0]*48}px -${spriteCoords[1]*48}px;"></div>
     </a>
     `
     $(`#upgrade-store`).html($(`#upgrade-store`).html() + this.store_html)
@@ -21,45 +50,47 @@ class Upgrade {
 
   buy(game) {
     if(game.cookies < (this.price)) return false
+    if(this.owned == true) return false
+
     game.cookies -= (this.price)
     this.owned = true
     this.run(game);
     game.updateCps();
     $(`#inventory-${this.name.toLowerCase()}`).append(this.inventory_item_html)
-
     return true
   }
 }
 class Building {
-  constructor(name, price, cps, inventory_row_enabled=true) {
+  constructor(name, price, cps, spriteCoords=[0,0], inventory_row_enabled=true) {
     this.name = name;
+    this.refname = name.toLowerCase().split(" ").join("")
     this.price = price;
     this.cps = cps;
     this.inventory_row_enabled = inventory_row_enabled;
 
     this.have = 0;
 
-    this.id = `buy-${this.name.toLowerCase()}`;
-    this.inventory_id = `inventory-${this.name.toLowerCase()}`
+    this.id = `buy-${this.refname}`;
+    this.inventory_id = `inventory-${this.refname}`
 
     this.inventory_item_html = 
       `
-      <div class="inventory-item ${this.name.toLowerCase()}">
-        <img src="https://orteil.dashnet.org/cookieclicker/img/${this.name.toLowerCase()}.png">
+      <div class="inventory-item ${this.refname}">
+        <img src="https://orteil.dashnet.org/cookieclicker/img/${this.refname}.png">
       </div>
       `
     this.inventory_html = 
       `
-      <div id="${this.inventory_id}" class="inventory-row ${this.name.toLowerCase()}"></div>
+      <div id="${this.inventory_id}" class="inventory-row ${this.refname}" style="background: url(https://orteil.dashnet.org/cookieclicker/img/${this.refname}Background.png)"></div>
       <div class="horizontal-divider"/>
       `
     this.store_html = 
       `
-      <div id="${this.id}" class="store-row ${this.name.toLowerCase()} disabled">
-        <div class="store-img"></div>
+      <div id="${this.id}" class="store-row ${this.refname} disabled" title="${this.have} ${this.name}(s) producing ${this.cps} cookies per second.">
+        <div class="store-img" style="background: url('img/buildings.png') ${spriteCoords[0] * 64}px -${spriteCoords[1] * 64}px;"></div>
         <div class="store-info">
           <h3>${this.name}</h3>
-          <p class="price">${this.price}</p>
+          <p class="price">${new Num(this.price).named()}</p>
         </div>
         <h1 class="have">${this.have}</h1>
       </div>
@@ -71,7 +102,8 @@ class Building {
   }
   updatePrice(price) {
     this.price = Math.round(price);  
-    $(`#${this.id} .store-info .price`).html(this.price)
+    $(`#${this.id}`).attr("title", `${this.have} ${this.name}(s) producing ${this.cps} cookies per second.`)
+    $(`#${this.id} .store-info .price`).html(new Num(this.price).named())
     return true
   }
   // buy qty of building, given the game object
@@ -96,15 +128,19 @@ class Game {
     this.update_interval = 20; // cookie updates per second
     this.incfac = 1.15; // factor by which the price of buildings increases by each purchase
     this.upgrades = {
-      moreClicks: new Upgrade("moreClicks", "double cursor production", 100, (game) => { game.buildings.cursor.cps *= 2 }),
+      moreclicks: new Upgrade("More Clicks", "Double cursor production.", 500, (game) => { game.buildings.cursor.cps *= 2;     game.buildings.cursor.updatePrice(game.buildings.cursor.price);      }, [0,0]),
+      prunejuice: new Upgrade("Prune Juice", "Double grandma production.", 50000, (game) => { game.buildings.grandma.cps *= 2;     game.buildings.grandma.updatePrice(game.buildings.grandma.price);      }, [1,0]),
     }
     this.buildings = {
-      cursor: new Building("Cursor", 10, 0.1, false),
-      grandma: new Building("Grandma", 25, 5),
-      farm: new Building("Farm", 35, 7),
-      mine: new Building("Mine", 500, 15),
-      factory: new Building("Factory", 12000, 55),
-      bank: new Building("Bank", 30000, 100)
+      cursor: new Building("Cursor", 10, 0.1, [0,0] , false),
+      grandma: new Building("Grandma", 100, 5, [0,1]),
+      farm: new Building("Farm", 1100, 15, [0,3]),
+      mine: new Building("Mine", 12000, 55, [0,4]),
+      factory: new Building("Factory", 130000, 100, [0,5]),
+      bank: new Building("Bank", 1400000, 500, [0,6]),
+      temple: new Building("Temple", 20000000, 1500, [0,7]),
+      wizardtower: new Building("Wizard Tower", 200000000, 3000, [0,8]),
+      shipment: new Building("Shipment", 2000000000, 6000, [0,9]),
     }
   }
 
@@ -137,6 +173,16 @@ class Game {
       }
       else {
         $(`#buy-${b}`).addClass("disabled")
+      }
+    }
+
+    for(const u in this.upgrades) {
+      const upgrade = this.upgrades[u]
+      if(this.canAfford(upgrade, 1)) {
+        $(`#buyupgrade-${u}`).removeClass("disabled")
+      }
+      else {
+        $(`#buyupgrade-${u}`).addClass("disabled")
       }
     }
   }
@@ -185,12 +231,12 @@ class Game {
     }, duration)
   }
   updateCps() {
-    $("#cookies-persecond").html(`${(this.cps).toFixed(1)} cookies per second`)
+    $("#cookies-persecond").html(`${new Num((this.cps).toFixed(1)).named()} cookies per second`)
   }
   updateCookies(options={}) {
     const { amount=this.cps, factor=1 } = options;
     this.cookies += (amount * factor);
-    $("#cookie-counter").html(`${Math.floor(this.cookies)} cookies`);
+    $("#cookie-counter").html(`${new Num(Math.floor(this.cookies)).named()} cookies`);
   }
 }
 
